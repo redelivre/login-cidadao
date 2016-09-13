ee90402a346fe3b42eb55f34dbd8446f8a78e38f
Guilherme Donato <guilhermednt@users.noreply.github.com>
2016-09-13 15:23:53 -0300

    added checks to warn if required_channel is not https #39

--- src/LoginCidadao/CoreBundle/EventListener/CheckDeployEventSubscriber.php
+++ src/LoginCidadao/CoreBundle/EventListener/CheckDeployEventSubscriber.php
@@ -7,11 +7,13 @@ use Doctrine\ORM\EntityManager;
 use LoginCidadao\CoreBundle\Entity\CityRepository;
 use LoginCidadao\OAuthBundle\Entity\Client;
 use LoginCidadao\OAuthBundle\Entity\ClientRepository;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
-class CheckDeployEventSubscriber implements EventSubscriberInterface
+class CheckDeployEventSubscriber implements EventSubscriberInterface, LoggerAwareInterface
 {
     const CHECK_DEPLOY_KEY = 'check_deploy';
 
@@ -27,13 +29,21 @@ class CheckDeployEventSubscriber implements EventSubscriberInterface
     /** @var string */
     private $defaultClientUid;
 
+    /** @var string */
+    private $requiredChannel;
+
+    /** @var LoggerInterface */
+    private $logger;
+
     public function __construct(
         EntityManager $em,
-        $defaultClientUid
+        $defaultClientUid,
+        $requiredChannel
     ) {
         $this->cityRepository = $em->getRepository('LoginCidadaoCoreBundle:City');
         $this->clientRepository = $em->getRepository('LoginCidadaoOAuthBundle:Client');
         $this->defaultClientUid = $defaultClientUid;
+        $this->requiredChannel = $requiredChannel;
 
         $this->cache = null;
     }
@@ -47,16 +57,33 @@ class CheckDeployEventSubscriber implements EventSubscriberInterface
     }
 
     /**
+     * Sets a logger instance on the object
+     *
+     * @param LoggerInterface $logger
+     * @return null
+     */
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
      * {@inheritDoc}
      */
     public static function getSubscribedEvents()
     {
         return array(
-            KernelEvents::REQUEST => 'checkDeploy',
+            KernelEvents::REQUEST => 'onRequest',
         );
     }
 
-    public function checkDeploy(GetResponseEvent $event)
+    public function onRequest(GetResponseEvent $event)
+    {
+        $this->checkRequiredChannel();
+        $this->checkDeploy();
+    }
+
+    private function checkDeploy()
     {
         if (null === $this->cache || $this->cache->contains(self::CHECK_DEPLOY_KEY)) {
             return;
@@ -71,6 +98,20 @@ class CheckDeployEventSubscriber implements EventSubscriberInterface
             throw new \RuntimeException('Make sure you did run the populate database command.');
         } else {
             $this->cache->save(self::CHECK_DEPLOY_KEY, true);
+        }
+    }
+
+    private function checkRequiredChannel()
+    {
+        if ($this->requiredChannel === 'https') {
+            return;
+        }
+
+        $message = "The parameter 'required_channel' should be set to 'https'! This is a CRITICAL SECURITY issue. Ignore this message only if you are _ABSOLUTELY_ sure you know what you are doing.";
+        if ($this->logger !== null) {
+            $this->logger->critical($message);
+        } else {
+            throw new \RuntimeException($message);
         }
     }
 }

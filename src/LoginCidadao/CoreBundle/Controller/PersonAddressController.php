f44d686ff10f548185b56ed1032a07ef4ab5a288
Guilherme Donato <guilhermednt@users.noreply.github.com>
2017-08-07 12:44:04 -0300

    code cleanup PROCERGS#714

--- src/LoginCidadao/CoreBundle/Controller/PersonAddressController.php
+++ src/LoginCidadao/CoreBundle/Controller/PersonAddressController.php
@@ -1,22 +1,27 @@
 <?php
+/**
+ * This file is part of the login-cidadao project or it's bundles.
+ *
+ * (c) Guilherme Donato <guilhermednt on github>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace LoginCidadao\CoreBundle\Controller;
 
+use Doctrine\Common\Collections\Collection;
 use Symfony\Component\HttpFoundation\Request;
+use LoginCidadao\CoreBundle\Entity\City;
+use LoginCidadao\CoreBundle\Entity\PersonAddress;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use LoginCidadao\CoreBundle\Entity\PersonAddress;
-use LoginCidadao\CoreBundle\Form\Type\RemovePersonAddressFormType;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Doctrine\Common\Collections\Collection;
-use Symfony\Component\Form\FormError;
-use LoginCidadao\CoreBundle\Entity\State;
-use LoginCidadao\CoreBundle\Entity\City;
 
 class PersonAddressController extends Controller
 {
-
     /**
      * @Route("/person/addresses", name="lc_person_addresses")
      * @Template()
@@ -35,7 +40,8 @@ class PersonAddressController extends Controller
     public function newAddressAction(Request $request)
     {
         $address = new PersonAddress();
-        $form    = $this->createForm('LoginCidadao\CoreBundle\Form\Type\PersonAddressFormType', $address);
+        $formType = 'LoginCidadao\CoreBundle\Form\Type\PersonAddressFormType';
+        $form = $this->createForm($formType, $address);
 
         $form->handleRequest($request);
 
@@ -52,101 +58,43 @@ class PersonAddressController extends Controller
         return compact('form', 'deleteForms');
     }
 
-    private function checkAddressLocation(&$address, $form, &$em)
-    {
-        $form = $form->get('location');
-        if (!$address->getCountry()) {
-            $form->get('country')->addError(new FormError($this->get('translator')->trans('required.field')));
-            return false;
-        }
-        $isPreferred = $em->getRepository('LoginCidadaoCoreBundle:Country')->isPreferred($address->getCountry());
-        if (!$address->getState() && !$isPreferred) {
-            $steppe = ucwords(strtolower(trim($form->get('statesteppe')->getData())));
-            if ($steppe) {
-                $repo = $em->getRepository('LoginCidadaoCoreBundle:State');
-                $ent  = $repo->findOneBy(array(
-                    'name' => $steppe,
-                    'country' => $address->getCountry()
-                ));
-                if (!$ent) {
-                    $ent = new State();
-                    $ent->setName($steppe);
-                    $ent->setCountry($address->getCountry());
-                    $em->persist($ent);
-                }
-                $address->setState($ent);
-            }
-        }
-        if (!$address->getState()) {
-            $form->get('state')->addError(new FormError($this->get('translator')->trans('required.field')));
-            return false;
-        }
-        if (!$address->getCity() && !$isPreferred) {
-            $steppe = ucwords(strtolower(trim($form->get('citysteppe')->getData())));
-            if ($address->getState()) {
-                $state = $address->getState();
-            } elseif (isset($ent)) {
-                $state = $ent;
-            } else {
-                $state = null;
-            }
-            if ($state && $steppe) {
-                $repo = $em->getRepository('LoginCidadaoCoreBundle:City');
-                $ent  = $repo->findOneBy(array(
-                    'name' => $steppe,
-                    'state' => $state
-                ));
-                if (!$ent) {
-                    $ent = new City();
-                    $ent->setName($steppe);
-                    $ent->setState($state);
-                    $em->persist($ent);
-                }
-                $address->setCity($ent);
-            }
-        }
-        if (!$address->getCity()) {
-            $form->get('city')->addError(new FormError($this->get('translator')->trans('required.field')));
-            return false;
-        }
-        return true;
-    }
-
     /**
      * @Route("/person/addresses/{id}/edit", name="lc_person_addresses_edit")
      * @Template("LoginCidadaoCoreBundle:PersonAddress:newAddress.html.twig")
      */
     public function editAction(Request $request, $id)
     {
-        $em        = $this->getDoctrine()->getManager();
-        $addresses = $em->getRepository('LoginCidadaoCoreBundle:PersonAddress');
-        $address   = $addresses->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $address = $this->findOr404($id);
         if ($address->getPerson()->getId() !== $this->getUser()->getId()) {
             throw new AccessDeniedException();
         }
         if ($address->getId() > 0) {
             $city = $address->getCity();
             if ($city instanceof City) {
-                $state   = $city->getState();
+                $state = $city->getState();
                 $country = $state->getCountry();
                 $address->getLocation()->setCity($city)
                     ->setState($state)->setCountry($country);
             }
         }
 
-        $form = $this->createForm('LoginCidadao\CoreBundle\Form\Type\PersonAddressFormType', $address);
+        $formType = 'LoginCidadao\CoreBundle\Form\Type\PersonAddressFormType';
+        $form = $this->createForm($formType, $address);
         $form->handleRequest($request);
 
         if ($form->isValid()) {
             $address->setPerson($this->getUser());
 
             $em->flush();
+
             return $this->redirect($this->generateUrl('lc_person_addresses'));
         }
-        $deleteForms = $this->getDeleteForms();
-        $edit_form   = $form->createView();
 
-        return compact('edit_form', 'deleteForms');
+        return [
+            'edit_form' => $form->createView(),
+            'deleteForms' => $this->getDeleteForms(),
+        ];
     }
 
     /**
@@ -156,53 +104,74 @@ class PersonAddressController extends Controller
     public function deleteAction(Request $request, $id)
     {
         $translator = $this->get('translator');
-        $form       = $this->createForm('LoginCidadao\CoreBundle\Form\Type\RemovePersonAddressFormType');
+        $formType = 'LoginCidadao\CoreBundle\Form\Type\RemovePersonAddressFormType';
+        $form = $this->createForm($formType);
         $form->handleRequest($request);
 
         if ($form->isValid()) {
-            $person    = $this->getUser();
-            $em        = $this->getDoctrine()->getManager();
-            $addresses = $em->getRepository('LoginCidadaoCoreBundle:PersonAddress');
-            $address   = $addresses->find($id);
-
             try {
+                $address = $this->findOr404($id);
+                $person = $this->getUser();
                 if ($address->getPerson()->getId() !== $person->getId()) {
                     throw new AccessDeniedException();
                 }
+                $em = $this->getDoctrine()->getManager();
                 $em->remove($address);
                 $em->flush();
             } catch (AccessDeniedException $e) {
-                $this->get('session')->getFlashBag()->add('error',
-                    $translator->trans("Access Denied."));
+                $this->get('session')->getFlashBag()->add(
+                    'error',
+                    $translator->trans("Access Denied.")
+                );
             } catch (\Exception $e) {
-                $this->get('session')->getFlashBag()->add('error',
-                    $translator->trans("Wasn't possible to remove this address."));
-                $this->get('session')->getFlashBag()->add('error',
-                    $e->getMessage());
+                $this->get('session')->getFlashBag()->add(
+                    'error',
+                    $translator->trans("Wasn't possible to remove this address.")
+                );
+                $this->get('session')->getFlashBag()->add(
+                    'error',
+                    $e->getMessage()
+                );
             }
         } else {
-            $this->get('session')->getFlashBag()->add('error',
-                $translator->trans("Wasn't possible to remove this address."));
+            $this->get('session')->getFlashBag()->add(
+                'error',
+                $translator->trans("Wasn't possible to remove this address.")
+            );
         }
 
         return $this->redirect($this->generateUrl('lc_person_addresses'));
     }
 
-    protected function getDeleteForms()
+    private function getDeleteForms()
     {
-        $person      = $this->getUser();
+        $person = $this->getUser();
         $deleteForms = array();
-        $addresses   = $person->getAddresses();
+        $addresses = $person->getAddresses();
 
         if (is_array($addresses) || $addresses instanceof Collection) {
             foreach ($addresses as $address) {
-                $data                           = array('address_id' => $address->getId());
+                $data = array('address_id' => $address->getId());
                 $deleteForms[$address->getId()] = $this->createForm(
-                        'LoginCidadao\CoreBundle\Form\Type\RemovePersonAddressFormType',
-                        $data)
-                    ->createView();
+                    'LoginCidadao\CoreBundle\Form\Type\RemovePersonAddressFormType',
+                    $data
+                )->createView();
             }
         }
+
         return $deleteForms;
+    }
+
+    private function findOr404($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('LoginCidadaoCoreBundle:PersonAddress');
+
+        $address = $repo->find($id);
+        if (!$address instanceof PersonAddress) {
+            throw new NotFoundHttpException('PersonAddress not found');
+        }
+
+        return $address;
     }
 }
